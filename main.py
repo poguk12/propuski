8ebee92da0bf4bfecc3b1a4cfa9ed2c2ff3e5d15
Аина <okrodik@gmail.com>
2025-04-04 01:18:35 +1100

    Добавил БД

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import date
+import sqlite3
 
 list = {
 
@@ -6,33 +7,67 @@ list = {
 
 dt_now = date.today()
 
-print(dt_now)
+db = sqlite3.connect('propusk.db')
+cursor = db.cursor()
 
+#cursor.execute("""
+#CREATE TABLE propuski (
+#    Date text,
+#    Prichina text,
+#    Prepod text
+#)""")
+
+#cursor.execute("INSERT INTO propuski VALUES ('date', 'prichina', 'teacher')")
+
+cursor.execute("SELECT rowid, * FROM propuski WHERE rowid > 2")
+
+items = cursor.fetchall()
+
+for el in items:
+    print(el[1] + " - " + el[3])
+
+db.commit()
+
+db.close()
 
 def add_propusk(date, prichina, teacher):
 
-    if list.get(dt_now) is not None:
-        list[date].append(prichina)
+    if date in list:
+        list[date].append(prichina  + " " + teacher)
     else:
-        list[date] = [prichina + " " + teacher]
+        list[date] = []
+        list[date].append(prichina + " " + teacher)
 
+    print("Пропуск ", prichina, " добавлена на дату ", date)
 
-while True:
+rodik = True
+
+while rodik:
 
     slovo = input("Введите задание: ")
 
-    if("add" == slovo):
+    if "add" == slovo :
         date = input("Введите дату пропуска: ")
         prichina = input("Введите причину пропуска: ")
         teacher = input("Введите имя или предмет пропуска: ")
 
-        if(date == "Seg"):
+        if date == "Сегодня" :
             date = dt_now
 
         add_propusk(date, prichina, teacher)
 
-    elif ("show" == slovo):
-        print(list)
+    elif "show" == slovo:
+        date = input("Введите дату для отображения списка задач: ")
+
+        if date in list:
+            print("На дату", date, " есть несколько пропусков: ")
+            for task in list[date]:
+                print('- ', task)
+        else:
+            print("Нет задач на эту дату")
+
+    elif "exit" == slovo:
+        rodik = False
 
 
 
