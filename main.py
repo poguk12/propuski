eef367a7188981ccb7a0a69466f6d359a71f4c2d
Аина <okrodik@gmail.com>
2025-10-07 00:01:24 +1100

    добавил БД чутьчуть изменил код

--- main.py
+++ main.py
@@ -1,73 +1,41 @@
 from datetime import date
 import sqlite3
 
-list = {
+propuski_list = {}
 
-}
+dt_now = date.today().strftime('%Y-%m-%d')
 
-dt_now = date.today()
 
-db = sqlite3.connect('propusk.db')
-cursor = db.cursor()
+def BD(prichina, namePrepod):
 
-#cursor.execute("""
-#CREATE TABLE propuski (
-#    Date text,
-#    Prichina text,
-#    Prepod text
-#)""")
+    with sqlite3.connect('db/database.db') as db:
+        cursor = db.cursor()
+    
+        query = """INSERT INTO propuski (Date, Prichina, Prepod) VALUES(?, ?, ?)"""
+        values = (dt_now, prichina, namePrepod)
 
-#cursor.execute("INSERT INTO propuski VALUES ('date', 'prichina', 'teacher')")
+        cursor.execute(query, values)
 
-cursor.execute("SELECT rowid, * FROM propuski WHERE rowid > 2")
+        cursor.execute("SELECT * FROM propuski")
 
-items = cursor.fetchall()
+        db.commit()
 
-for el in items:
-    print(el[1] + " - " + el[3])
+def Vivod():
+ with sqlite3.connect('db/database.db') as db:
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM propuski")
+        rows = cursor.fetchall()
 
-db.commit()
-
-db.close()
-
-def add_propusk(date, prichina, teacher):
-
-    if date in list:
-        list[date].append(prichina  + " " + teacher)
-    else:
-        list[date] = []
-        list[date].append(prichina + " " + teacher)
-
-    print("Пропуск ", prichina, " добавлена на дату ", date)
+        for row in rows:
+            print(row)
 
 rodik = True
 
 while rodik:
+    prichina = input("Введите причину пропуска: ")
+    teacher = input("Введите имя или предмет пропуска: ")
 
-    slovo = input("Введите задание: ")
-
-    if "add" == slovo :
-        date = input("Введите дату пропуска: ")
-        prichina = input("Введите причину пропуска: ")
-        teacher = input("Введите имя или предмет пропуска: ")
-
-        if date == "Сегодня" :
-            date = dt_now
-
-        add_propusk(date, prichina, teacher)
-
-    elif "show" == slovo:
-        date = input("Введите дату для отображения списка задач: ")
-
-        if date in list:
-            print("На дату", date, " есть несколько пропусков: ")
-            for task in list[date]:
-                print('- ', task)
-        else:
-            print("Нет задач на эту дату")
-
-    elif "exit" == slovo:
-        rodik = False
-
-
-
+    if prichina == "1":
+         Vivod()
+    else:
+        BD(prichina, teacher)
